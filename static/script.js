9874ce121d966b1208bd1741f98c873579a7b127
João Pedro <joaopedro1santiago@gmail.com>
2022-04-29 21:27:01 -0300

    Seção sobre a API criada com ancoragem.

--- static/script.js
+++ static/script.js
@@ -8,3 +8,15 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 })
+
+/* Fechar o menu qunado clicar em um item do menu */
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
