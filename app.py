f66d467eb308fd733c5bd6aa09f72e9678ccbeb4
João Pedro <joaopedro1santiago@gmail.com>
2022-04-30 17:14:36 -0300

    Testando aplicação web

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+import secrets
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.sqlite3'
@@ -8,18 +9,23 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+secret = secrets.token_urlsafe(32)
+
+app.secret_key = secret
+
+
 class Modelo(db.Model):
-    id = db.Column(db.Integer(), primary_key = True)
-    nome = db.Column(db.String(255), nullable = False)
-    descricao = db.Column(db.Text(), nullable = False)
+    id = db.Column(db.Integer(), primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    descricao = db.Column(db.Text(), nullable=False)
 
     def __repr__(self):
         return self.nome
-    
+
     @classmethod
     def get_all(cls):
         return cls.query.all()
-    
+
     @classmethod
     def get_by_nome(cls, nome):
         return cls.query.filter_by(nome=nome).first()
@@ -27,7 +33,7 @@ class Modelo(db.Model):
     @classmethod
     def get_by_id(cls, id):
         return cls.query.get_or_404(id)
-    
+
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -36,12 +42,14 @@ class Modelo(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+
 class ModeloSchema(Schema):
     id = fields.Integer()
     nome = fields.String()
     descricao = fields.String()
 
-@app.route('/modelo', methods = ['GET'])
+
+@app.route('/modelo', methods=['GET'])
 def get_all_modelos():
     """Retorna todos os modelos de IA cadastrados na base de dados."""
     modelos = Modelo.get_all()
@@ -51,27 +59,42 @@ def get_all_modelos():
         dados
     )
 
-@app.route('/modelo', methods = ['POST'])
+
+@app.route('/modelo', methods=['POST'])
 def create_modelo():
     """Cadastra um novo modelo no banco de dados."""
-    dados = request.get_json()
+    content_type = request.headers.get('Content-Type')
 
-    novo_modelo = Modelo(
-        nome = dados.get('nome'),
-        descricao = dados.get('descricao')
-    )
+    if (content_type == 'application/json'):
+        dados = request.get_json()
 
-    novo_modelo.save()
+        novo_modelo = Modelo(
+            nome=dados.get('nome'),
+            descricao=dados.get('descricao')
+        )
 
-    serializer = ModeloSchema()
-    
-    dados = serializer.dump(novo_modelo)
+        novo_modelo.save()
 
-    return jsonify(
-        dados
-    ), 201
+        serializer = ModeloSchema()
 
-@app.route('/modelo/<string:nome>', methods = ['GET'])
+        dados = serializer.dump(novo_modelo)
+
+        return jsonify(
+            dados
+        ), 201
+    elif (content_type == 'application/x-www-form-urlencoded'):
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+
+        novo_modelo = Modelo(nome=nome, descricao=descricao)
+        novo_modelo.save()
+
+        flash("Modelo adicionado com sucesso!")
+
+        return redirect(url_for('index'))
+
+
+@app.route('/modelo/<string:nome>', methods=['GET'])
 def get_modelo_by_nome(nome):
     """Retorna nome e descrição de um determinado modelo passado seu nome como parâmetro."""
     modelo = Modelo.get_by_nome(nome)
@@ -84,7 +107,8 @@ def get_modelo_by_nome(nome):
         dados
     ), 200
 
-@app.route('/modelo/<int:id>', methods = ['GET'])
+
+@app.route('/modelo/<int:id>', methods=['GET'])
 def get_modelo_by_id(id):
     """Retorna nome e descrição de um determinado modelo passado seu id como parâmetro."""
     modelo = Modelo.get_by_id(id)
@@ -97,7 +121,8 @@ def get_modelo_by_id(id):
         dados
     ), 200
 
-@app.route('/modelo/<int:id>', methods = ['PUT'])
+
+@app.route('/modelo/<int:id>', methods=['PUT'])
 def update_modelo(id):
     """Edita os campos de um modelo (nome, descrição)."""
     modelo = Modelo.get_by_id(id)
@@ -105,7 +130,7 @@ def update_modelo(id):
     dados = request.get_json()
 
     modelo.nome = dados.get('nome')
-    modelo.descricao = modelo.get('descricao')
+    modelo.descricao = dados.get('descricao')
 
     db.session.commit()
 
@@ -117,7 +142,8 @@ def update_modelo(id):
         dados
     ), 200
 
-@app.route('/modelo/<int:id>', methods = ['DELETE'])
+
+@app.route('/modelo/<int:id>', methods=['DELETE'])
 def delete_modelo(id):
     """Deleta o modelo da base de dados."""
     modelo = Modelo.get_by_id(id)
@@ -128,14 +154,41 @@ def delete_modelo(id):
         {"message": "Deletado"}
     ), 204
 
+
 @app.before_first_request
 def create_table():
     db.create_all()
+
 
 @app.route('/')
 def index():
     modelos = Modelo.get_all()
     return render_template('index.html', modelos=modelos)
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if (request.method == 'POST'):
+        dados = Modelo.query.get(request.form.get('id'))
+
+        dados.nome = request.form['nome']
+        dados.descricao = request.form['descricao']
+
+        db.session.commit()
+        flash("Modelo editado com sucesso!")
+
+        return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:id>/', methods=['GET', 'POST'])
+def delete(id):
+    dados = Modelo.query.get(id)
+    db.session.delete(dados)
+    db.session.commit()
+    flash("Modelo Deletado com Sucesso.")
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
